f676cb42b3422325c9808ef0ba51ac0b50ff9709
ottojolanki <otaljo@gmail.com>
2025-10-06 14:09:41 -0700

    DSERV-1063-cloudfront-east-certs (#12)

--- src/shared_infrastructure/igvf_catalog_prod/domain.py
+++ src/shared_infrastructure/igvf_catalog_prod/domain.py
@@ -16,6 +16,11 @@ class Domain(Construct):
             'DomainCertificate',
             'arn:aws:acm:us-west-2:636503752262:certificate/623d26db-c86c-4341-b7cd-b5c9f92b2ffb',
         )
+        self.east1_certificate = Certificate.from_certificate_arn(
+            self,
+            'East1Certificate',
+            'arn:aws:acm:us-east-1:636503752262:certificate/b8fcae0e-ef8a-40a8-9dfb-1ab977d97505',
+        )
         self.zone = HostedZone.from_lookup(
             self,
             'DomainZone',
--- src/shared_infrastructure/igvf_dev/domain.py
+++ src/shared_infrastructure/igvf_dev/domain.py
@@ -16,6 +16,11 @@ class DemoDomain(Construct):
             'DomainCertificate',
             'arn:aws:acm:us-west-2:109189702753:certificate/6bee1171-2028-43eb-aab8-d992da3c60df'
         )
+        self.east1_certificate = Certificate.from_certificate_arn(
+            self,
+            'East1Certificate',
+            'arn:aws:acm:us-east-1:109189702753:certificate/1db73b67-1246-4e2a-9e32-cc4cea331fb1'
+        )
         self.zone = HostedZone.from_lookup(
             self,
             'DomainZone',
